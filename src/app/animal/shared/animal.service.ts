450de200b0e3484853f97b5a31ec827f1258982c
Robin Coma Delperier <robin@placeme.io>
2020-09-24 17:28:06 +0200

    feat(Env): api url config

--- src/app/animal/shared/animal.service.ts
+++ src/app/animal/shared/animal.service.ts
@@ -2,30 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Animal } from './animal';
-
-const api = 'http://localhost:3000';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AnimalService {
   constructor(private httpClient: HttpClient) {}
 
   get(id: number): Observable<Animal> {
-    return this.httpClient.get<Animal>(`${api}/animals/${id}`);
+    return this.httpClient.get<Animal>(`${environment.api}/animals/${id}`);
   }
 
   findAll(): Observable<Animal[]> {
-    return this.httpClient.get<Animal[]>(`${api}/animals`);
+    return this.httpClient.get<Animal[]>(`${environment.api}/animals`);
   }
 
   delete(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${api}/animals/${id}`);
+    return this.httpClient.delete<void>(`${environment.api}/animals/${id}`);
   }
 
   save(animal: Animal): Observable<Animal> {
-    return this.httpClient.post<Animal>(`${api}/animals`, animal);
+    return this.httpClient.post<Animal>(`${environment.api}/animals`, animal);
   }
 
   update(animal: Animal): Observable<void> {
-    return this.httpClient.put<void>(`${api}/animals/${animal.id}`, animal);
+    return this.httpClient.put<void>(
+      `${environment.api}/animals/${animal.id}`,
+      animal
+    );
   }
 }
